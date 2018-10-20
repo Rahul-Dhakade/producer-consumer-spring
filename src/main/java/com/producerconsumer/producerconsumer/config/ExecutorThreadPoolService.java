d27f2d48f4f6d32725e5056b7c19b9bf5a50d14d
rahul-dhakade <rahul10.pd@gmail.com>
2018-10-20 22:38:49 +0530

    bean configured taskexecutor

--- src/main/java/com/producerconsumer/producerconsumer/config/ExecutorThreadPoolService.java
+++ src/main/java/com/producerconsumer/producerconsumer/config/ExecutorThreadPoolService.java
@@ -3,6 +3,7 @@ package com.producerconsumer.producerconsumer.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -16,6 +17,24 @@ public class ExecutorThreadPoolService {
     public ExecutorService executorThreadPoolService(){
         ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * THREADS_MULTIPLE_PER_CORE);
         return executorService;
+    }
+
+
+    @Bean
+    public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
+        executor.setCorePoolSize(4);
+
+        executor.setMaxPoolSize(4);
+
+        executor.setThreadNamePrefix("default_task_executor_thread");
+
+        executor.initialize();
+
+        return executor;
+
     }
 
 }
